cadcd2256f39dc131b7bf13fa7b050be37ac7d36
kkiarie <kkiarie4@gmail.com>
2022-02-13 02:00:44 +0300

    Loan Strutcure

---
+++ database/migrations/2022_02_12_225703_create_loans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('loans', function (Blueprint $table) {
+            $table->id();
+            $table->integer("user_id")->nullable();
+            $table->decimal("amount",15,2)->nullable();
+            $table->date("due_date")->nullable();
+            $table->decimal("loan_interest",15,2)->nullable();
+            $table->integer("repayment_status")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('loans');
+    }
+};
